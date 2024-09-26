f9ead31c6e6b5bf3ccd6eea0cd393d9af7adb63a
Tyuiu.SeledkovNP <Tyuiu.SeledkovNP@outlook.com>
2024-09-26 15:21:20 +0500

    Завершено задание Task 1

---
+++ Tyuiu.SeledkovNP.Sprint1.Task2.V6/Program.cs
@@ -0,0 +1,51 @@
+﻿using Tyuiu.SeledkovNP.Sprint1.Task2.V6.Lib;
+
+namespace Tyuiu.SeledkovNP.Sprint1.Task2.V6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Селедков Н. П. | Смартб-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #6                                                              *");
+            Console.WriteLine("* Выполнил: Селедков Никита Павлович | Смартб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результат по формуле (x+y)/(3*y) и печатает его на экране.    *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+         
+
+            double x, y;
+            
+            Console.WriteLine("Ваше значение X");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ваше значение Y");
+            y = Convert.ToDouble(Console.ReadLine());
+
+           
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate(x, y));
+            
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
