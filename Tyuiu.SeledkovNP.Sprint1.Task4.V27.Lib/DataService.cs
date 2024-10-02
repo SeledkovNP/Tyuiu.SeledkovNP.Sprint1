ee8eadd584d588eeabd172e0fefa70a6731202cc
Tyuiu.SeledkovNP <Tyuiu.SeledkovNP@outlook.com>
2024-10-02 10:45:14 +0500

    Bag Fix

--- Tyuiu.SeledkovNP.Sprint1.Task4.V27.Lib/DataService.cs
+++ Tyuiu.SeledkovNP.Sprint1.Task4.V27.Lib/DataService.cs
@@ -6,8 +6,11 @@ namespace Tyuiu.SeledkovNP.Sprint1.Task4.V27.Lib
     {
         public double Calculate(double x, double y)
         {
-            var res = 1 + (Math.Sin(x * Math.PI)) / (x - Math.Sqrt(Math.Abs(y)));
+            var res = (1 + Math.Sin(x * Math.PI)) / (x - Math.Sqrt(Math.Abs(y)));
             return res;
+
+
+
         }
     }
 }
